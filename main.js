3a7289a2f334d4e4a4d51537c90786cc44f4298c
Pritesh Zaver <priteshzaver@gmail.com>
2022-12-20 09:29:03 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const clay = ["Clay", "Clay", "Clay", "Clay"];
+const toFireInKiln = [];
+
+for (const pottery of clay) {
+    const mug = `${pottery} coffee mug`
+    toFireInKiln.push(mug)
+};
+
+console.log(toFireInKiln);
---
+++ readme.md
@@ -0,0 +1,1 @@
+The purpose of this project is to use the .push method and a for...of  loop.
